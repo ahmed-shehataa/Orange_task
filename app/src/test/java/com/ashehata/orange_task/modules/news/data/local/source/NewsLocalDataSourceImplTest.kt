6bd27120ce0933b4058402f0e0d9901b5b3a006d
ahmed shehata <ahamdyshehata7@gmail.com>
2023-08-16 10:27:29 +0300

    Update NewsLocalDataSourceImplTest

--- app/src/test/java/com/ashehata/orange_task/modules/news/data/local/source/NewsLocalDataSourceImplTest.kt
+++ app/src/test/java/com/ashehata/orange_task/modules/news/data/local/source/NewsLocalDataSourceImplTest.kt
@@ -3,45 +3,76 @@ package com.ashehata.orange_task.modules.news.data.local.source
 import com.ashehata.orange_task.modules.news.data.local.dao.NewsDao
 import com.ashehata.orange_task.modules.news.data.model.NewsDataModel
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito
-import org.mockito.kotlin.times
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
+import org.mockito.kotlin.verify
 
 class NewsLocalDataSourceImplTest {
 
+
+    @get:Rule
+    var rule: MockitoRule = MockitoJUnit.rule()
+
+    @Mock
+    private lateinit var newsDao: NewsDao
+
+    private lateinit var newsLocalDataSource: NewsLocalDataSourceImpl
+
     private val newsList = List(20) { NewsDataModel(url = "dummy", title = "apple company") }
-    private var newsDao: NewsDao = Mockito.mock()
-    private var newsLocalDataSourceImpl = NewsLocalDataSourceImpl(newsDao)
+
+    @Before
+    fun setup() {
+        newsLocalDataSource = NewsLocalDataSourceImpl(newsDao)
+    }
 
     @Test
-    fun `getAllNews()`() {
-        Mockito.`when`(newsDao.getAll(1, 20, "apple")).thenReturn(newsList)
-        val actual = newsLocalDataSourceImpl.getAllNews(1, 20, "apple")
-        assertEquals(newsList, actual)
+    fun `getAllNews should call dao getAll method with correct arguments`() {
+        // arrange
+        val page = 1
+        val perPage = 10
+        val keyword = "apple"
+
+        // act
+        newsLocalDataSource.getAllNews(page, perPage, keyword)
+
+        // assert
+        verify(newsDao).getAll(page, perPage, keyword)
+    }
+
+    @Test
+    fun `insertNews should call dao insertAll method with correct arguments`() = runBlocking {
+        // act
+        newsLocalDataSource.insertNews(newsList)
+
+        // assert
+        verify(newsDao).insertAll(newsList)
     }
 
 
     @Test
-    fun `insertNews()`() = runBlocking {
-        newsLocalDataSourceImpl.insertNews(newsList)
-        Mockito.verify(newsDao, times(1)).insertAll(newsList)
+    fun `deleteNews should call dao delete method with correct argument`() = runBlocking {
+        // arrange
+        val urlToDelete = "dummy"
+
+        // act
+        newsLocalDataSource.deleteNews(urlToDelete)
+
+        // assert
+        verify(newsDao).delete(urlToDelete)
     }
 
     @Test
-    fun `deleteNews()`() = runBlocking {
-        val news = NewsDataModel(url = "dummy", title = "apple company")
-        newsLocalDataSourceImpl.deleteNews(news.url)
-        Mockito.verify(newsDao, times(1)).delete(news.url)
+    fun `clearAllNews should call dao deleteAll method`() = runBlocking {
+        // act
+        newsLocalDataSource.clearAllNews()
+
+        // assert
+        verify(newsDao).deleteAll()
     }
-
-    @Test
-    fun `clearAllNews()`() = runBlocking {
-        newsLocalDataSourceImpl.clearAllNews()
-        Mockito.verify(newsDao, times(1)).deleteAll()
-    }
-
-
 
 
 }
